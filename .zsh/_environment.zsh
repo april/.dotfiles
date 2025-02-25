33db45e086445eb317c9e57b825883d5c11e6970
April King <april@grayduck.mn>
2025-02-24 20:33:22 -0600

    fix typo for secure transport

--- .zsh/_environment.zsh
+++ .zsh/_environment.zsh
@@ -3,7 +3,7 @@ export EDITOR=vim
 export ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
 if [[ `uname` == "Darwin" ]]; then
-  export CURL_SSL_BACKGROUND=secure-transport
+  export CURL_SSL_BACKEND=secure-transport
   export PATH=/opt/homebrew/bin:/opt/homebrew/opt/ruby/bin:$PATH
 fi
 
